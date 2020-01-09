3412804daf0908e916a22096f8ced6441dba94d4
x0rzkov <x0rzkov@protonmail.com>
2020-01-09 14:44:29 +0000

    cleanup

--- client/client.js
+++ client/client.js
@@ -3,14 +3,14 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, './public');
 const app = express();
 
 const server = http.createServer(app);
 
 app.use(express.static(publicPath));
 
-//Starting server on port 4242
+// Starting server on port 4242
 server.listen(4242, () => {
     console.log("Server started on port 4242");
 });
